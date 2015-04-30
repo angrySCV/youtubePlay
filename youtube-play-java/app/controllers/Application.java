b578bc99fb9e3bdbb617bbe476339477df85bb97
angrySCV <dima@angryscv.ru>
2015-05-01 02:34:01 +0300

    some change

--- youtube-play-java/app/controllers/Application.java
+++ youtube-play-java/app/controllers/Application.java
@@ -1,14 +1,17 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+//        Logger.info("hello from app");
+Logger.warn("warning message");
+        return ok(index.render("new message"));
+
     }
 
 }
